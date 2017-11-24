2e020e5a3e3f053dd33088792d9527816b389499
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-11-24 22:20:00 +0000

    fetch put

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,25 +1,56 @@
 
 var button = document.querySelector('#start-button');
 var output = document.querySelector('#output');
+var output2 = document.querySelector('#output2');
 
 button.addEventListener('click', function() {
   console.log("Clicked!")
   // Create a new Promise here and use setTimeout inside the function you pass to the constructor
-  var promise = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-      resolve('https://swapi.co/api/people/1')
-    }, 3000)
-  })
+  // var promise = new Promise((resolve, reject)=>{
+  //   setTimeout(()=>{
+  //     resolve('https://swapi.co/api/people/1')
+  //   }, 3000)
+  // })
 
-  promise.then((url)=>{
-    fetch(url)
-    .then((response)=>response.json())
-    .then((data)=>{
-      output.innerHTML=data.name
+  // promise
+  //   .then((url)=>{
+  //     fetch(url)
+  //     .then((response)=>response.json())
+  //     .then((data)=>{
+  //       output.innerHTML=data.name
+  //     })
+  //   })
+  //   .catch((err)=>{
+  //     output.innerHTML=err
+  //   })
+
+  var promise2 = new Promise(function(resolve, reject){
+    setTimeout(function(url){
+      resolve('https://httpbin.org/put')
     })
   })
 
-  
+  promise2.then(function(url){
+    fetch(url,{
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      mode: 'cors',
+      body: JSON.stringify({person: {name: "Noby", age: "anonymous"}})
+    })
+      .then(function(response){return response.json()})
+      .then(function(data){
+        console.log(data)
+        var resultStr = "Name: "+data.json.person.name+" Age: "+data.json.person.age
+        output2.innerHTML=resultStr
+      })
+      .catch(function(err){
+        
+        output.innerHTML=err
+      })
+  })
 
   // Handle the Promise "response" (=> the value you resolved) and return a fetch()
   // call to the value (= URL) you resolved (use a GET request)
